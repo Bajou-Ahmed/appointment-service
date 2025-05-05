7c9a4878a889ecb64fd3bb28d979137af246f5a7
Moustaid <nourelhoudamoustaid@gmail.com>
2025-05-05 16:14:09 +0100

    premier commit

---
+++ src/test/java/com/emsi/appointment_service/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emsi.appointment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
